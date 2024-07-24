1688444cc8af702a88f23e51866a66185738556b
Aurelien0192 <mosini.aurelien@gmail.com>
2024-07-24 13:56:43 +0200

    style : add gradient to name place in place little cards

--- src/Application/ComplexeComponents/PlaceDisplayLittleCards.tsx
+++ src/Application/ComplexeComponents/PlaceDisplayLittleCards.tsx
@@ -10,7 +10,9 @@ export const PlaceDisplayLittleCard:React.FC<propsPlaceDisplayLittleCard> = (pro
         <NavLink to='' className=" flex flex-col gap-2 shadow rounded-xl w-full h-[396px] p-[10px]">
             <div className="relative h-64">
                 <img className="rounded-xl object-cover size-full" src={"http://localhost:3001/"+props.place.getImage()[0].path} />
-                <p className="px-4 py-2 absolute bottom-0 text-2xl font-bold text-white">{props.place.getName()}</p>
+                <p 
+                    className="px-4 py-2 w-full rounded-b-xl absolute bottom-0 text-2xl font-bold text-white bg-gradient-to-t from-black"
+                >{props.place.getName()}</p>
             </div>
             <div className="flex flex-col justify-between h-28">
                 <p className="font-bold">{props.place.getCity()}</p>
