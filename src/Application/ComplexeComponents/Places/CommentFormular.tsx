9c88c0c0c32fcba71829bcf0924947ded1e4db6c
Aurelien0192 <mosini.aurelien@gmail.com>
2024-08-08 19:33:19 +0200

    refactoring : create DatePickerCalendar

--- src/Application/ComplexeComponents/Places/CommentFormular.tsx
+++ src/Application/ComplexeComponents/Places/CommentFormular.tsx
@@ -1,16 +1,13 @@
-import { DatePicker } from "@mantine/dates"
 import { HotelCategorieOrNotationSelector } from "./HotelCategorieOrNotationSelector"
 import { TextArea } from "../../Components/General/TextArea"
 import { Button, Checkbox } from "@mantine/core"
 import { useState } from "react"
-import { useClickOutside } from "@mantine/hooks"
 import { comment } from "../../../Module/Comment/comment.type"
 import { FormularServices } from "../../../Module/FormularGeneralServices/formularServices"
 import { CommentService } from "../../../Module/Comment/comment.service"
 import { Place } from "../../../Module/Place/Place.class"
 import { useSelector } from "../../../Module/HotelCategorieOrNotationSelector/HotelCategorieSelectorOrNotation.hook"
-import calendarLogo from '../../../public/Logo/calendar.svg'
-import { Input } from "../../Components/General/Input"
+import { DatePickerCalendar } from "../../Components/General/DatePickerCalendar"
 
 type CommentFormularProps = {
     dataOnePlace: Place
@@ -18,12 +15,9 @@ type CommentFormularProps = {
 
 export const CommentFormular:React.FC<CommentFormularProps> = (props) =>{
 
-    const [hidden, setHidden] = useState<boolean>(true)
-    const ref = useClickOutside(() => setHidden(true))
     const [msg, setMsg] = useState<string>("")
     const [submitDisabled, setSubmitDisabled] = useState<boolean>(true)
     const {selectedNoteOrHotelCategorie} = useSelector()
-    const [dateVisit, setDateVisite] = useState<Date | null>(null)
 
     const changeMsgComment = async (e:React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
@@ -43,36 +37,24 @@ export const CommentFormular:React.FC<CommentFormularProps> = (props) =>{
 
     return(
         <form onSubmit={(e) => changeMsgComment(e)} className="w-full flex flex-col gap-8 items-end">
-                            <div className="w-full flex flex-col gap-3">
-                                <h2 className="text-2xl font-bold">Comment qualifiez-vous votre expérience?</h2>
-                                <HotelCategorieOrNotationSelector type="circle" labelHidden={true} />
-                            </div>
-                            <div className="relative w-full flex flex-col gap-3">
-                                <h2 className="text-2xl font-bold">Quand y êtes-vous allé?</h2>
-                                <Input 
-                                    icon={calendarLogo} 
-                                    onClickIcon={()=>{setHidden(false)}}
-                                    flexDirection="flex-col"
-                                    type="date" 
-                                    name="dateVisited" 
-                                    value={dateVisit && dateVisit?.toLocaleDateString().split('/').reverse().join('-')} 
-                                    placeholder=""
-                                    positionIcon="end"
-                                />
-                                <div ref={ref} className={`${hidden && "hidden"} absolute top-16 bg-white rounded-xl shadow`}>
-                                    <DatePicker value={dateVisit} onChange={setDateVisite} />
-                                </div>
-                            </div>
-                            <div className="flex flex-col gap-3 w-full">
-                                <h2 className="text-2xl font-bold">Ajouter un commentaire</h2>
-                                <TextArea flexDirection="flex-col" size="md" placeholder="J'ai adoré ce lieu. Tout était parfait. Je recommande fortement..." name="comment"/>
-                            </div>
-                            <div className="flex gap-4">
-                                <Checkbox onChange={(e)=>{changeDisabled(e)}} name="accept" color="#D98D30" variant="outline" size="md"/>
-                                <p>Je certifie que cet avis reflète ma propre expérience et mon opinion authentique. Je certifie également que je n’ai aucun lien professionnel ou personnel avec cet organisme et que je n’ai reçu aucune compensation financière ou autre de sa part pour rédiger cet avis. </p>
-                            </div>
-                            <p className="text-red-500">{msg}</p>
-                            <Button disabled={submitDisabled} type="submit">Envoyer l'avis</Button>
-                        </form>
+            <div className="w-full flex flex-col gap-3">
+                <h2 className="text-2xl font-bold">Comment qualifiez-vous votre expérience?</h2>
+                <HotelCategorieOrNotationSelector type="circle" labelHidden={true} />
+            </div>
+            <div className="relative w-full flex flex-col gap-3">
+                <h2 className="text-2xl font-bold">Quand y êtes-vous allé?</h2>
+                <DatePickerCalendar />
+            </div>
+            <div className="flex flex-col gap-3 w-full">
+                <h2 className="text-2xl font-bold">Ajouter un commentaire</h2>
+                <TextArea flexDirection="flex-col" size="md" placeholder="J'ai adoré ce lieu. Tout était parfait. Je recommande fortement..." name="comment"/>
+            </div>
+            <div className="flex gap-4">
+                <Checkbox onChange={(e)=>{changeDisabled(e)}} name="accept" color="#D98D30" variant="outline" size="md"/>
+                <p>Je certifie que cet avis reflète ma propre expérience et mon opinion authentique. Je certifie également que je n’ai aucun lien professionnel ou personnel avec cet organisme et que je n’ai reçu aucune compensation financière ou autre de sa part pour rédiger cet avis. </p>
+            </div>
+            <p className="text-red-500">{msg}</p>
+            <Button disabled={submitDisabled} type="submit">Envoyer l'avis</Button>
+        </form>
     )
 }
