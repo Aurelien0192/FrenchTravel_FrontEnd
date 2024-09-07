8a5257736181abe743516f71b1cfb49a1dae677e
Aurelien <mosini.aurelien@gmail.com>
2024-09-07 15:12:48 +0200

    fix: all modal in comments viewer open in same time

---
+++ src/Application/ComplexeComponents/Comment/CommentCards.tsx
@@ -0,0 +1,77 @@
+import { IoCheckmarkCircleOutline } from "react-icons/io5"
+import { Comment } from "../../../Module/Comment/comment.class"
+import { Like } from "../../Components/svg/Like"
+import { HotelCategorieOrNoteShow } from "../Places/HotelCategorieOrNotationShow"
+import { CustomModal } from "../../Components/General/CustomModal"
+import { Button } from "../../Components/General/Button"
+import { TextArea } from "../../Components/General/TextArea"
+import { useDisclosure } from "@mantine/hooks"
+import { CommentResponseCard } from "./CommentResponseCard"
+
+type CommentCardProps = {
+    index: number
+    comment: Comment
+    variant:1|3
+    msg:string
+    onSubmit: React.FormEventHandler<HTMLFormElement>
+    LikeComment: React.MouseEventHandler<HTMLButtonElement>
+}
+
+
+export const CommentCard:React.FC<CommentCardProps> = (props) => {
+
+    const [RespondCommentModal, RespondCommentModalManager] = useDisclosure()
+
+    return(
+        <div key={props.index} className="flex flex-col gap-3 items-end w-full">
+            {!props.comment.getIsResponse() && 
+                <div  className={`flex w-full flex-col md:flex-row gap-2 ${props.variant===3?" px-2.5 py-1.5 md:gap-2.5 rounded-xl shadow":"flex flex-col md:gap-0 p-2.5"}`} >
+                    <div className="flex md:flex-col gap-4 border-b-2 md:border-r-2 md:border-b-0 border-black w-72">
+                        <div>
+                            <img className="size-10 rounded-full object-cover" src={props.comment.getProfilePhoto()} />
+                            <p>{props.comment.getUsernamePoster()}</p>
+                        </div>
+                        <p className="text-sm">{`publié le : ${props.comment.getCreateAt()}`}</p>
+                        <p className="text-sm">{`visité le : ${props.comment.getDateVisited()}`}</p>
+                    </div>
+                    <div className="pl-6 flex flex-col w-full justify-between">
+                        <div className="flex justify-between items-start">
+                            <HotelCategorieOrNoteShow type="circle" categorie={props.comment.getNote()} />
+                            {props.variant===3 && <p>{props.comment.getPlaceName()}</p>}
+                        </div>
+                        <p>{props.comment.getComment()}</p>
+                        <div className="flex justify-between">
+                            <div className="flex gap-2 items-center">
+                                <p>{props.comment.getLike()}</p>
+                                <button className="w-fit" onClick={props.LikeComment}>
+                                    <Like liked={props.comment.getLiked()} />
+                                </button>
+                            </div>
+                            {props.variant === 3 &&
+                                props.comment.getResponse()?
+                                <div className="flex gap-2 items-center">
+                                    <IoCheckmarkCircleOutline size="30px" color="#22c55e" />
+                                    <p className="text-green-500" >Commentaire répondu</p>
+                                </div>
+                                : props.variant === 3 && <Button size="xs" onClick={RespondCommentModalManager.open}>Répondre</Button>}
+                        </div>
+                        <CustomModal opened={RespondCommentModal} onClose={RespondCommentModalManager.close} size="lg">
+                            <form onSubmit={props.onSubmit} className="flex flex-col gap-3 items-end">
+                                <p className="w-full">Commentaires:</p>
+                                <p className="rounded shadow p-2.5">{props.comment.getComment()}</p>
+                                <TextArea label="Réponse :" flexDirection="flex-col" placeholder="Merci pour ce commentaire" name="comment" size="md" />
+                                <Button type ="submit" size="xs">Envoyer</Button>
+                                <p>{props.msg}</p>
+                            </form>
+                        </CustomModal>
+                    </div>
+                </div>
+            }
+            {props.comment.getResponse() &&
+                <div className="md:w-10/12">
+                    <CommentResponseCard response={props.comment.getResponse() as Comment}/>
+                </div>    
+            }
+        </div>
+    )
+}
--- src/Application/ComplexeComponents/Comment/CommentsViewer.tsx
+++ src/Application/ComplexeComponents/Comment/CommentsViewer.tsx
@@ -1,20 +1,15 @@
 import { Pagination } from "@mantine/core"
-import { HotelCategorieOrNoteShow } from "../Places/HotelCategorieOrNotationShow"
 import { useComment } from "../../../Module/Comment/comment.hook"
 import { Like } from "../../Components/svg/Like"
-import { Button } from "../../Components/General/Button"
-import { useDisclosure } from "@mantine/hooks"
-import { TextArea } from "../../Components/General/TextArea"
 import { FormularServices } from "../../../Module/FormularGeneralServices/formularServices"
 import { comment } from "../../../Module/Comment/comment.type"
 import { useState } from "react"
 import { CommentService } from "../../../Module/Comment/comment.service"
 import { CommentResponseCard } from "./CommentResponseCard"
 import { Comment } from "../../../Module/Comment/comment.class"
-import { IoCheckmarkCircleOutline } from "react-icons/io5"
 import { CommentFilter } from "./CommentFilter"
 import { FrenchTravelAnimated } from "../../Components/svg/FrenchTravelAnimated"
-import { CustomModal } from "../../Components/General/CustomModal"
+import { CommentCard } from "./CommentCards"
 
 type commentsViewerProps={
     visitor_id:string|null
@@ -28,7 +23,6 @@ type commentsViewerProps={
 export const CommentsViewer:React.FC<commentsViewerProps> = (props) => {
 
     const {page, commentsTab, numberOfElement, changeNotationChoice, changePage, LikeAComment} = useComment(props.idOfPlaceOrUser, props.findBy,props.visitor_id?props.visitor_id:null, props.search?props.search:null)
-    const [RespondCommentModal, RespondCommentModalManager] = useDisclosure()
     const [msg, setMsg] = useState<string>("")
          
     const changeMsgComment = async (e:React.FormEvent<HTMLFormElement>, comment_id:string) => {
@@ -41,6 +35,7 @@ export const CommentsViewer:React.FC<commentsViewerProps> = (props) => {
       setMsg(newMsgComment)
       console.log(commentsTab)
     }
+
     if (commentsTab){
         return(
             <div className="flex flex-col-reverse md:flex-row-reverse md:gap-32 w-full">
@@ -48,56 +43,7 @@ export const CommentsViewer:React.FC<commentsViewerProps> = (props) => {
                     {commentsTab.length!==0 ? commentsTab.map((comment, index)=>{
                         if(props.variant === 1 || props.variant === 3){
                             return(
-                                <div key={index} className="flex flex-col gap-3 items-end w-full">
-                                    {!comment.getIsResponse() && 
-                                        <div  className={`flex w-full flex-col md:flex-row gap-2 ${props.variant===3?" px-2.5 py-1.5 md:gap-2.5 rounded-xl shadow":"flex flex-col md:gap-0 p-2.5"}`} >
-                                            <div className="flex md:flex-col gap-4 border-b-2 md:border-r-2 md:border-b-0 border-black w-72">
-                                                <div>
-                                                    <img className="size-10 rounded-full object-cover" src={comment.getProfilePhoto()} />
-                                                    <p>{comment.getUsernamePoster()}</p>
-                                                </div>
-                                                <p className="text-sm">{`publié le : ${comment.getCreateAt()}`}</p>
-                                                <p className="text-sm">{`visité le : ${comment.getDateVisited()}`}</p>
-                                            </div>
-                                            <div className="pl-6 flex flex-col w-full justify-between">
-                                                <div className="flex justify-between items-start">
-                                                    <HotelCategorieOrNoteShow type="circle" categorie={comment.getNote()} />
-                                                    {props.variant===3 && <p>{comment.getPlaceName()}</p>}
-                                                </div>
-                                                <p>{comment.getComment()}</p>
-                                                <div className="flex justify-between">
-                                                    <div className="flex gap-2 items-center">
-                                                        <p>{comment.getLike()}</p>
-                                                        <button className="w-fit" onClick={()=>{LikeAComment(comment.getId())}}>
-                                                            <Like liked={comment.getLiked()} />
-                                                        </button>
-                                                    </div>
-                                                    {props.variant === 3 &&
-                                                        comment.getResponse()?
-                                                        <div className="flex gap-2 items-center">
-                                                            <IoCheckmarkCircleOutline size="30px" color="#22c55e" />
-                                                            <p className="text-green-500" >Commentaire répondu</p>
-                                                        </div>
-                                                        : props.variant === 3 && <Button size="xs" onClick={RespondCommentModalManager.open}>Répondre</Button>}
-                                                </div>
-                                                <CustomModal opened={RespondCommentModal} onClose={RespondCommentModalManager.close} size="lg">
-                                                    <form onSubmit={(e)=>{changeMsgComment(e, comment.getId())}} className="flex flex-col gap-3 items-end">
-                                                        <p className="w-full">Commentaires:</p>
-                                                        <p className="rounded shadow p-2.5">{comment.getComment()}</p>
-                                                        <TextArea label="Réponse :" flexDirection="flex-col" placeholder="Merci pour ce commentaire" name="comment" size="md" />
-                                                        <Button type ="submit" size="xs">Envoyer</Button>
-                                                        <p>{msg}</p>
-                                                    </form>
-                                                </CustomModal>
-                                            </div>
-                                        </div>
-                                    }
-                                    {comment.getResponse() &&
-                                        <div className="md:w-10/12">
-                                            <CommentResponseCard response={comment.getResponse() as Comment}/>
-                                        </div>    
-                                    }
-                                </div>
+                                <CommentCard index= {index} comment={comment} variant={props.variant} msg={msg} onSubmit={(e)=>{changeMsgComment(e, comment.getId())}} LikeComment={()=>{LikeAComment(comment.getId())}} />
                             )
                         }else{
                             return(
