6257c87c00b8b2978e3d270b04171b0435987076
Aurelien <mosini.aurelien@gmail.com>
2024-09-09 19:13:26 +0200

    fix : update user don't update display

--- src/Module/UpdateFormular/UpdateFormularUser.service.ts
+++ src/Module/UpdateFormular/UpdateFormularUser.service.ts
@@ -17,9 +17,11 @@ export class UpdateFormularService{
     const data: userToUpdate = JSON.parse(JSON.stringify(formJson))
     let responseOfServer:AxiosResponse = await UserServices.updateUser(data) as AxiosResponse
     if(responseOfServer.status===200){
-      responseOfServer = {...JSON.parse(sessionStorage.getItem("UserAuthentifiate") as string), responseOfServer}
+      responseOfServer.data.token = JSON.parse(sessionStorage.getItem("UserAuthentifiate")as string).data.token
+      responseOfServer.data.email = data.email
       console.log(responseOfServer)
       const newUser: AuthentifiateUser = AuthentifiateUser.createAuthentifiateUser(responseOfServer)
+      console.log(newUser)
       sessionStorage.setItem("UserAuthentifiate",JSON.stringify(responseOfServer))
       authentificationService.updateAuthentifiateUser(newUser)
     }
